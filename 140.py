764b2d0b1915a39b0fc124a215c5da4d6a71b479
Laurent <busugu20218@gmail.com>
2026-03-25 15:30:25 -0500

    Commit 2: 6 to 10.

--- 140.py
+++ 140.py
@@ -1,10 +1,11 @@
-#Write a Python program to print "Hello Python"
+# 1 -  Write a Python program to print "Hello Python"
 print('Hello World.')
 
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-#Write a Python program to do arithmetical operations addition and division
+# 2 -  Write a Python program to do arithmetical operations addition and division
 num1 = float(input("Enter num1: "))
 num2 = float(input("Enter num2: "))
 sum = num1 + num2 
@@ -18,17 +19,22 @@ else:
 
 
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-#Write a Pvthon program to find the area of a triangle.
+
+
+# 3 -  Write a Pvthon program to find the area of a triangle.
 base = float(input('Enter base: '))
 height = float(input('Enter height: '))
 triangleArea = (base * height)/2
 print('The area of the triangle is ' + str(triangleArea))
 
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-#Write a Python program to swan two variables
+
+# 4 -  Write a Python program to swan two variables
 a = input('Enter a: ')
 b = input('Enter b: ')
 print(a, b)
@@ -50,13 +56,116 @@ print(temp)
 # Output: False
 
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-#Write a Python program to generate a random number
+
+# 5 -  Write a Python program to generate a random number
 import random
 print(random.randint(1, 100000000000))
 
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+# 6 -  Write a Python program to convert Kilometers to miles:
+
+x_Kilometers = float(input("Enter the distance in kilometers: "))
+
+# We know that: 1 Kilometers = 0.621371 mile -----> we'll call the conversion_factor f 
+
+f = 0.621371
+
+x_miles = f * x_Kilometers 
+
+print(str(x_Kilometers) + " km equals to " + str(x_miles) + " miles.")
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+# 7 - Write a Python proaram to convert Celsius to Fahrenheit
+
+x_celsius = float(input("Enter the temperature: "))
+
+# (0C * 9/5) +32 = 32F
+
+x_Fara = (x_celsius * 9/5) + 32
+
+print(str(x_celsius) + " is " + str(x_Fara) + " Fahrenheit.")
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# 8 - Write a Puthon program to displav calendar.
+
+import calendar
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+cal = calendar.month(year, month)
+print(cal)
+
+
+
+
+# Write a Python program to display calendar with the exact date =========
+#========== NOT CORRECT ===========
+import calendar
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+day = int(input("Enter day: "))
+
+cal = calendar.month(year, month, day)
+print(cal)
+
+
+
+
+
+#========== This is correct ==============
+import datetime
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+day = int(input("Enter day: "))
+
+date = datetime.date(year, month, day)
+print(date.strftime("%A"))  # prints the day of the week
+
+
+
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# 9 - Write a Python program to solve quadratic equation.
+
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# 10 - Write a Python program to swan two variables without temp variable.
+
+var1 = int(input("Enter year: "))
+var2 = int(input("Enter year: "))
+
+print("var1 = "+ str(var2))
+print("var1 = "+ str(var1))
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# 11 - Write a Python Program to Check if a Nurr is Positive. Negative or Zero
 
 
 
@@ -64,5 +173,33 @@ print(random.randint(1, 100000000000))
 
 
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+#12
+
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#13
+
+
+
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#14
+
+
+
+
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#15
 
